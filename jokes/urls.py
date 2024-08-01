ff26a4b8586131c1d66f7a85176cbe6f0faec081
WaWa5305 <wandaakers5305@gmail.com>
2024-08-01 05:05:52 -0500

    changing jokes to use slugs

--- jokes/urls.py
+++ jokes/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from .views import ( JokeCreateView, JokeDeleteView, JokeDetailView, JokeListView, JokeUpdateView)
 
 app_name = 'jokes'
-urlpatterns =  [
-   path('joke/<int:pk>/delete/', JokeDeleteView.as_view(), name='delete'),
-   path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
-   path('joke/create/', JokeCreateView.as_view(), name='create'),
-   path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
-   path('', JokeListView.as_view(), name='list'),
-   
+urlpatterns = [
+    path('joke/<slug>/update/', JokeUpdateView.as_view(), name='update'),
+    path('joke/<slug>/delete/', JokeDeleteView.as_view(), name='delete'),
+    path('joke/create/', JokeCreateView.as_view(), name='create'),
+    path('joke/<slug>/', JokeDetailView.as_view(), name='detail'),
+    path('', JokeListView.as_view(), name='list'),
 ]
 
